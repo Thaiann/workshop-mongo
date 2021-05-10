ab3b9889191e2dc88cb8c8f7458f38c598c79674
Thaiann Hernandes <thaiann.rhygor@gmail.com>
2021-05-10 10:18:29 -0300

    Workshop-mongo

---
+++ src/main/java/com/thaiann/workshopmongo/services/UserService.java
@@ -0,0 +1,52 @@
+package com.thaiann.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thaiann.workshopmongo.domain.User;
+import com.thaiann.workshopmongo.dto.UserDTO;
+import com.thaiann.workshopmongo.repository.UserRepository;
+import com.thaiann.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delet(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getEmail());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}
